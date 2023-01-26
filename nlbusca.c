e18dc0d77280f324e07c484bf4bfb8f9d920c02f
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-01-25 21:21:58 -0300

    Update nlbusca

--- nlbusca.c
+++ nlbusca.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "exception.h"
+#include "hash_table.h"
+#include "parser.h"
+#include "repo_indices.h"
 
 int main(int argc, char const *argv[]) {
-    printf("TODO: Pesquisando, %d, [%s]...\n", argc, argv[0]);
+    if (argc != 3)
+        exception_throw(
+            "ArgumentException",
+            "Couldn't find command line argument for binary file or K-onstant",
+            EXIT_FAILURE);
+
+    int *k = parseInt(argv[2]);
+
+    Indice *idx = repoidx_carregaIndice(argv[1]);
+
+    // TODO: Fazer o menu para selecionar entre as funções do programa (buscar, classificar, etc.)
+
+    free(k);
+    indice_dispose(idx);
+
     return EXIT_SUCCESS;
 }
