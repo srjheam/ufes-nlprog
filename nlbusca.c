abd0cbfa06d72e149b834dd867b823cbf1164e1e
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-01-31 20:53:34 -0300

    Update nlbusca

--- nlbusca.c
+++ nlbusca.c
@@ -18,7 +18,40 @@ int main(int argc, char const *argv[]) {
 
     Indice *idx = repoidx_carregaIndice(argv[1]);
 
-    // TODO: Fazer o menu para selecionar entre as funções do programa (buscar, classificar, etc.)
+    printf("[q] - Buscar notícias\n"
+           "[c] - Classificar notícias\n"
+           "[f] - Relatório de palavra\n"
+           "[l] - Relatório de documentos\n"
+           "Escolha uma opção: ");
+
+    system("clear");
+
+    char op = '\0';
+    scanf("%c%*c", op);
+    switch (op) {
+    case 'q':
+        
+        break;
+
+    case 'c':
+        
+        break;
+
+    case 'f':
+
+        break;
+
+    case 'l':
+
+        break;
+
+    default:
+        free(k);
+        indice_dispose(idx);
+
+        exception_throw_failure("Opção inválida. Saindo do programa...\n");
+        break;
+    }
 
     free(k);
     indice_dispose(idx);
