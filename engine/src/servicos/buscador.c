1e96048e950bbce0da700aac1cae335ca93a4012
Tucozz <tutuestefanato@gmail.com>
2023-02-01 22:58:17 -0300

    add relatorio palavras

--- engine/src/servicos/buscador.c
+++ engine/src/servicos/buscador.c
@@ -13,6 +13,10 @@
 
 #include "buscador.h"
 
+static int cmp_tfidf_listanoticias(const KeyValuePair *x, const KeyValuePair *y){
+    return *(float *)kvp_get_value(y) - *(float *)kvp_get_value(x);;
+}
+
 Lista *BuscaNoticias(char *query, Indice *idx) {
     if (query[strlen(query) - 1] == '\n')
         query[strlen(query) - 1] = '\0';
@@ -48,6 +52,8 @@ Lista *BuscaNoticias(char *query, Indice *idx) {
     }
 
     Lista *lista_noticias = ht_to_list(documentos);
+
+    lista_ordena(lista_noticias, *cmp_tfidf_listanoticias);
 
     return lista_noticias;
 }
---
+++ engine/src/servicos/relatorio.c
@@ -0,0 +1,78 @@
+#include <libgen.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "documento.h"
+#include "exception.h"
+#include "extlib.h"
+#include "indice.h"
+#include "key_value_pair.h"
+#include "palavra.h"
+#include "ref_palavra.h"
+#include "repo_noticias.h"
+
+#include "relatorio.h"
+
+static int cmp_qtd_documentos(KeyValuePair *x, KeyValuePair *y){
+    return *(int *)refdoc_get_freq(kvp_get_value(y)) - *(int *)refdoc_get_freq(kvp_get_value(x));
+}
+
+static int cmp_qtd_classes(KeyValuePair *x, KeyValuePair *y){
+    return *(int *)kvp_get_value(y) - *(int *)kvp_get_value(x);
+}
+
+void relatorio_palavras(Indice *idx, char *query){
+    //conferir se a palavra existe
+    Palavra *palavra = NULL;
+    if(palavra = ht_get(indice_get_palavras(idx), query) == NULL){
+        printf("Palavra inválida ou não presente em nenhum documento.\n");
+        return;
+    }
+    RefDocumento *refs_docs = palavra_get_refDocumentos(palavra);
+
+    //numero total de documentos
+    printf("Quantidade de documentos em que a palavra está presente: %d\n\n",
+            ht_get_qty(refs_docs));
+    
+    //os 10 em que ela mais aparece
+    Lista *refdocs = ht_to_list(refs_docs);
+    lista_ordena(refdocs, (cmp_fn)cmp_qtd_documentos);
+
+    printf("10 documentos em que a palavra mais aparece:\n");
+    for(int i=0;i<10;i++){
+        printf("%d. %s\n", i, lista_get_elemento(refdocs, i));
+    }
+    printf("\n");
+
+    //frequencia da palavra por classe
+    // Hashtable <string, int>
+    HashTable *ht_classes = ht_init((cpy_fn)strdup, (cpy_fn)intdup, (cmp_fn)strcmp, (free_fn)free, (free_fn)free);
+    int *saveptr = calloc(1, sizeof *saveptr);
+    KeyValuePair *curr_refdoc = NULL;
+    while ((curr_refdoc = ht_iter(refs_docs, saveptr)) != NULL) {
+        char *doc_nome = refdoc_get_documento(kvp_get_value(curr_refdoc));
+        Documento *curr_class = doc_get_classe(ht_get(indice_get_documentos(idx), doc_nome));
+        
+        if(ht_get(ht_classes, curr_class) == NULL)
+        ht_add(ht_classes, curr_class, 0);
+
+        int *freq_ptr = ht_get(ht_classes, curr_class);
+        *(freq_ptr)++;
+    }
+    free(saveptr);
+
+    Lista *lista_classes = ht_to_list(ht_classes);
+    lista_ordena(lista_classes, (cmp_fn)cmp_qtd_classes);
+
+    printf("Frequência por classe:\n");
+    for(int i = 0; i < lista_get_quantidade(lista_classes); i++){
+        printf("Classe: %s - Frequência: %d\n",
+                            kvp_get_key(lista_get_elemento(lista_classes, i)),
+                            kvp_get_value(lista_get_elemento(lista_classes, i)));
+    }
+
+}
+
+void relatorio_documentos(){
+
+}
