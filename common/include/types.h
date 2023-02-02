3440a8691861f05a1eb8546d037c7b0a71b005da
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-02-01 22:58:43 -0300

    Update types

--- common/include/types.h
+++ common/include/types.h
@@ -1,22 +1,31 @@
 #ifndef _TYPES_
 #define _TYPES_
 
+#include <stdlib.h>
+
 /**
- * @brief Tipo comum de uma função que libera uma instância qualquer alocado dinâmicamente na memória.
- * 
+ * @brief Tipo comum de uma função que libera uma instância qualquer alocado
+ * dinâmicamente na memória.
+ *
  */
 typedef void (*free_fn)(void *);
 
 /**
  * @brief Tipo comum de uma função que compara duas instâncias.
- * 
+ *
  */
 typedef int (*cmp_fn)(const void *, const void *);
 
 /**
  * @brief Tipo comum de uma função que copia uma instância.
- * 
+ *
  */
 typedef void *(*cpy_fn)(const void *);
+
+/**
+ * @brief Tipo comum de uma função que calcula o hash de uma instância.
+ * 
+ */
+typedef size_t (*hash_fn)(const void *);
 
 #endif
