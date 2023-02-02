ddcbbac2cd651d3fca25069871e29d4c2a979d72
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-02-01 23:00:53 -0300

    Add linked_list

---
+++ common/include/linked_list.h
@@ -0,0 +1,18 @@
+#ifndef _LINKED_LIST_
+#define _LINKED_LIST_
+
+#include "types.h"
+
+typedef struct tLinkedList LinkedList;
+
+LinkedList *ll_init(cpy_fn cpyValues, free_fn disposeValues);
+
+void ll_dispose(LinkedList *list);
+
+size_t ll_get_size(LinkedList *list);
+
+void ll_append(LinkedList *list, void *data);
+
+void *ll_iter(LinkedList *list, void **saveptr);
+
+#endif
---
+++ common/src/linked_list.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+
+#include "node.h"
+
+#include "linked_list.h"
+
+struct tLinkedList {
+    Node *head;
+    size_t size;
+
+    cpy_fn cpyValues;
+    free_fn disposeValues;
+};
+
+LinkedList *ll_init(cpy_fn cpyValues, free_fn disposeValues) {
+    LinkedList *list = malloc(sizeof *list);
+    if (list == NULL)
+        exception_throw_OutOfMemory("LinkedList malloc failed");
+
+    list->head = NULL;
+    list->size = 0;
+
+    list->cpyValues = cpyValues;
+    list->disposeValues = disposeValues;
+
+    return list;
+}
+
+void ll_dispose(LinkedList *list) {
+    Node *curr = list->head;
+    while (curr != NULL) {
+        Node *next = node_get_next(curr);
+
+        list->disposeValues(node_get_data(curr));
+        node_i_dispose(curr);
+
+        curr = next;
+    }
+
+    free(list);
+}
+
+size_t ll_get_size(LinkedList *list) { return list->size; }
+
+void ll_append(LinkedList *list, void *data) {
+    Node *node = node_init(data, list->head);
+    list->head = node;
+
+    list->size++;
+}
+
+void *ll_iter(LinkedList *list, void **saveptr) {
+    if (*saveptr == NULL) {
+        *saveptr = list->head;
+        return *saveptr;
+    }
+    
+    Node *next = node_get_next(*saveptr);
+    if (next == NULL)
+        return NULL;
+    
+    *saveptr = next;
+
+    return *saveptr;
+}
