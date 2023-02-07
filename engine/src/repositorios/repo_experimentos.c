4e5d0ede80abb144b725c41766a768c720ec5ab1
Jheam Storch Ross <jheam.storch@hotmail.com>
2023-02-07 14:57:03 -0300

    .

--- engine/src/repositorios/repo_experimentos.c
+++ engine/src/repositorios/repo_experimentos.c
@@ -57,27 +57,41 @@ void repoexp_salvaMatrizConfusaoTxt(const char *filename, HashTable *matriz,
         exception_throw("IOException", "Couldn't open file for writing",
                         EXIT_FAILURE);
 
-    fprintf(fp, "acc:%g\n", accuracy);
+    fprintf(fp, "Accuracy: %g\n", accuracy);
     fprintf(fp, "%c", '\n');
 
     LinkedList *classes = ht_keys(matriz);
 
-    fprintf(fp, "true\\predict\n");
-    fprintf(fp, "%c", '\n');
+    fprintf(fp, "True\\Predict");
 
     char *row = NULL;
     void *saveptr = NULL;
-    while ((row = ll_iter(classes, &saveptr)) != NULL){
-            fprintf(fp, "TESTE   %s", row);
+    while ((row = ll_iter(classes, &saveptr)) != NULL) {
+        fprintf(fp, " | %5s", row);
     }
-        
-        
+
+    fprintf(fp, "%c", '\n');
+
+    char c = '-';
+    int count = 12;
+    for (int i = 0; i < count; i++) {
+        fprintf(fp, "%c", c);
+    }
+
+    int n = ll_get_length(classes);
+    for (int i = 0; i < n; i++) {
+        fprintf(fp, "%s", "-|-");
+        int count = 5;
+        for (int j = 0; j < count; j++) {
+            fprintf(fp, "%c", c);
+        }
+    }
 
     fprintf(fp, "%c", '\n');
 
     saveptr = NULL;
     while ((row = ll_iter(classes, &saveptr)) != NULL) {
-        fprintf(fp, "%s", row);
+        fprintf(fp, "%12s", row);
 
         char *column = NULL;
         void *saveptr = NULL;
@@ -85,7 +99,7 @@ void repoexp_salvaMatrizConfusaoTxt(const char *filename, HashTable *matriz,
             HashTable *predictClasses = ht_get(matriz, row);
             int *value = ht_get(predictClasses, column);
 
-            fprintf(fp, ",%d", value == NULL ? 0 : *value);
+            fprintf(fp, " | %5d", value == NULL ? 0 : *value);
         }
 
         fprintf(fp, "%c", '\n');
